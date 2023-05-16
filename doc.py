8c9ea4820b73bc57dbb3593668257470fdd2e13a
Projects <65894626+amahha1@users.noreply.github.com>
2023-05-16 12:26:14 +0300

    Add files via upload

---
+++ doc.py
@@ -0,0 +1,30 @@
+VERSION = '0.3'
+
+HELP = '''
+
+------------------------------------------------------
+-Hama1c numbers from 4 to 10 digits as raw output:
+
+4 digits: from 0000 to 9999 
+5 digits: from 00009 to 99999
+...
+10 digits: from 0000000000 to 9999999999
+-Standard raw output is 7 digits
+
+-Allows to add any prefixes (e.g CC, NDC)
+
+-You have to set prefixes manually, just write them to the file "prefix.txt"
+
+-Each prefix should be written in a new line put your custom prefixes in the file "prefix.txt"
+
+$ python3 hama1c.py
+
+-Allows to add permanent suffix. Just put your suffix in the argument when script is launching.
+
+$ python3 hama1c.py [suffix]
+
+Quick launch:
+
+$ python3 hama1c.py [prefix] [suffix] [length]
+
+'''
